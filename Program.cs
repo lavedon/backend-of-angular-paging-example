86fa49337cd73d377461ff2a847b5034ba89abdc
Luke <lavedon@gmail.com>
2023-06-27 16:03:57 -0400

    updates

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         builder =>
         {
-            builder.WithOrigins("http://localhost:4200")
+            builder.WithOrigins("http://localhost:4200, https://localhost:4300")
                    .AllowAnyHeader()
                    .AllowAnyMethod();
         });
